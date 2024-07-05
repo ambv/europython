a90760f950a5ca742cb2d05af25c4d2ae91f5e67
Łukasz Langa <lukasz@langa.pl>
2024-07-05 15:14:08 +0200

    Separate Board to its own class, accept MIDI input and self-refresh when clock not running

--- europython.py
+++ europython.py
@@ -71,6 +71,82 @@ class Stopped(Exception):
     pass
 
 
+class Board(threading.Thread):
+    def __init__(self):
+        super().__init__(name="Board", daemon=True)
+
+        self.connected = False
+        self.coords = bytearray(b"\x00\x20\x29\x02\x10\x0A" + (b"\x00" * 130))
+        for pad_index in range(64):
+            pad_coord = self.index_to_pad(pad_index)
+            self.coords[6 + 2 * pad_index] = pad_coord
+        self.coords[6 + 128] = 0x63  # side LED
+        self.reset()
+
+        while LAUNCHPAD_PORT not in portmidi.get_output_names():
+            time.sleep(1)
+        self.output = portmidi.open_output(LAUNCHPAD_PORT)
+        # not connected yet, wait for input in run()
+
+    def run(self):
+        while LAUNCHPAD_PORT not in portmidi.get_input_names():
+            time.sleep(1)
+
+        counter = -1
+        input = portmidi.open_input(LAUNCHPAD_PORT)
+        self.connected = True
+        while True:
+            counter = (counter + 1) % 100
+            if not counter:
+                self.maybe_update()
+
+            message = input.poll()
+            if not message:
+                time.sleep(0.0001)
+                continue
+            if message.type == "note_off":
+                message.type = "note_on"
+                message.velocity = 0
+
+            if message.type == "note_on":
+                index = self.pad_to_index(message.note) + 1
+                if message.velocity:
+                    self.pad(index, 0x03)
+                else:
+                    self.pad(index, 0x00)
+
+    def clock_update(self):
+        self.self_update = False
+        self.update()
+
+    def maybe_update(self):
+        if self.self_update:
+            self.update()
+
+    def update(self):
+        self.output.send(SysEx(data=self.coords))
+
+    def reset(self):
+        self.self_update = True
+        self.coords[6 + 129] = 0x01  # side LED
+
+    def pad(self, number: int, color: int) -> None:
+        """`number` is 1-indexed. Color is Launchpad programmer's mode."""
+        self.coords[6 + 2 * (number - 1) + 1] = color
+
+    def index_to_pad(self, index: int) -> int:
+        """Argument is 0-indexed 8x8 grid coord. Result is Launchpad NOTE_ON note number."""
+        x = index % 8
+        y = index // 8
+        return 10 * y + x + 11
+
+    def pad_to_index(self, pad: int) -> int:
+        """Argument is Launchpad NOTE_ON note number. Result is 0-indexed 8x8 grid coord."""
+        x = (pad - 1) % 10
+        y = (pad - 1) // 10
+        return 8 * (y - 1) + x
+
+
 class Clock(threading.Thread):
     def __init__(self):
         super().__init__(name="Clock", daemon=True)
@@ -79,13 +155,8 @@ class Clock(threading.Thread):
         self.on_bar = threading.Event()
         self.countdown_lock = threading.Lock()
         self.countdowns = deque()
-        self.board = bytearray(b"\x00\x20\x29\x02\x10\x0A" + (b"\x00" * 130))
-        for pad_index in range(64):
-            pad_coord = self.index_to_pad(pad_index + 1)
-            self.board[6 + 2 * pad_index] = pad_coord
-        # side LED
-        self.board[6 + 128] = 0x63
-        self.board[6 + 129] = 0x01
+        self.board = Board()
+        self.board.start()
         self.reset()
 
     def reset(self):
@@ -95,8 +166,7 @@ class Clock(threading.Thread):
         self.position = -1  # pulses since last start
         self.beat = -1
         self.bar = -1
-        for pad_index in range(64):
-            self.board[6 + 2 * pad_index + 1] = pad_index
+        self.board.reset()
         with self.countdown_lock:
             for countdown in self.countdowns:
                 countdown.set()
@@ -107,17 +177,11 @@ class Clock(threading.Thread):
             time.sleep(1)
         input = portmidi.open_input(CLOCK_PORT)
 
-        while LAUNCHPAD_PORT not in portmidi.get_output_names():
-            time.sleep(1)
-        launchpad = portmidi.open_output(LAUNCHPAD_PORT)
-
         self.connected = True
-
-        launchpad.send(SysEx(data=self.board))
 
         for message in input:
             if message.type == "clock":
-                launchpad.send(SysEx(data=self.board))
+                self.board.clock_update()
                 if self.running:
                     self.tick()
             elif message.type in {"start", "continue"}:
@@ -125,16 +189,6 @@ class Clock(threading.Thread):
                 self.running = True
             elif message.type == "stop":
                 self.reset()
-                self.board[6 + 129] = 0x01
-                launchpad.send(SysEx(data=self.board))
-
-    def pad(self, number: int, color: int) -> None:
-        self.board[6 + 2 * (number - 1) + 1] = color
-
-    def index_to_pad(self, index: int) -> int:
-        x = (index - 1) % 8
-        y = (index - 1) // 8
-        return 10 * y + x + 11
 
     def tick(self) -> None:
         self.position = (self.position + 1) % 24
@@ -144,12 +198,12 @@ class Clock(threading.Thread):
             if self.beat == 0:
                 self.on_bar.set()
                 self.bar = (self.bar + 1) % 4
-                self.board[6 + 129] = 0x34
+                self.board.pad(65, 0x34)
             else:
-                self.board[6 + 129] = 0x35
+                self.board.pad(65, 0x35)
         else:
             if self.position == 12:
-                self.board[6 + 129] = 0x36
+                self.board.pad(65, 0x36)
             self.on_beat.clear()
             self.on_bar.clear()
         with self.countdown_lock:
@@ -169,6 +223,16 @@ class Clock(threading.Thread):
 
 
 clock = Clock()
+board = clock.board
+
+
+def method_on(cls_or_obj):
+    def attach_method(meth):
+        if not isinstance(cls_or_obj, type):
+            meth = meth.__get__(cls_or_obj, cls_or_obj.__class__)
+        setattr(cls_or_obj, meth.__name__, meth)
+
+    return attach_method
 
 
 class Seq(threading.Thread):
@@ -179,16 +243,19 @@ class Seq(threading.Thread):
 
     def play(self):
         for _ in range(4):
-            clock.pad(self.number, 0x03)
+            clock.board.pad(self.number, 0x03)
             clock.wait(6)
-            clock.pad(self.number, 0x40)
+            clock.board.pad(self.number, 0x40)
             clock.wait(6)
 
     def run(self):
-        clock.pad(self.number, 0x01)
-
         while True:
-            clock.on_bar.wait()
+            clock.board.pad(self.number, 0x40 if clock.running else 0x01)
+            try:
+                clock.on_bar.wait()
+            except Stopped:
+                continue
+
             try:
                 self.play()
             except Stopped:
